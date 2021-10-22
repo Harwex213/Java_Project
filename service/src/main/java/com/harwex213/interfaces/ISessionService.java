b2ef5bbc5963f11cdd5a50dc5e244005db7be70a
Harwex213 <harwex213@mail.ru>
2021-10-22 17:31:29 +0300

    feat(endpoints): sessions api

---
+++ service/src/main/java/com/harwex213/interfaces/ISessionService.java
@@ -0,0 +1,15 @@
+package com.harwex213.interfaces;
+
+import com.harwex213.dto.sessions.CreateSessionDto;
+import com.harwex213.dto.sessions.GetSessionDto;
+import com.harwex213.dto.sessions.UpdateSessionDto;
+import com.harwex213.exceptions.NotFoundException;
+
+import java.util.List;
+
+public interface ISessionService {
+    public List<GetSessionDto> getSessions();
+    public GetSessionDto createSession(CreateSessionDto createSessionDto) throws NotFoundException;
+    public void updateSession(UpdateSessionDto updateSessionDto) throws NotFoundException;
+    public void deleteSession(Long id) throws NotFoundException;
+}
